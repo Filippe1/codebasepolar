f38529363fe274f8be44e58ce1bdd436e26aa510
Filippe <19ffilippi@gmail.com>
2025-09-14 21:43:14 +0300

    small fix

---
+++ pages/dashboard/unprotected.js
@@ -0,0 +1,11 @@
+export default function ProtectedPage() {
+  
+  
+
+  return (
+    <div>
+      <h1>Unprotected Page</h1>
+      <p>You are logged in!</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="./login"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -51,15 +51,15 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Login
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="./signup"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Signup
           </a>
         </div>
       </main>
@@ -96,7 +96,7 @@ export default function Home() {
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="./dashboard/unprotected"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -107,7 +107,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to dashboard →
         </a>
       </footer>
     </div>
